f35a00af2614627bcf872b4b97b6948c9b69a99c
joykimmel <joykimmel@att.net>
2014-09-17 20:21:17 -0400

    Create fcc.py

---
+++ fcc.py
@@ -0,0 +1,18 @@
+from visual import *
+
+ed1 = sphere(radius = 0.4, pos =[0,0,0])
+ed2 = sphere(radius = 0.4, pos =[4,0,0])
+ed3 = sphere(radius = 0.4, pos =[4,0,4])
+ed4 = sphere(radius = 0.4, pos =[0,0,4])
+
+ed5 = sphere(radius=0.4, pos=[0,4,0])
+ed6 = sphere(pos =[4,4,0], radius=0.4)
+ed7 = sphere(pos=[4,4,4], radius=0.4)
+ed8 = sphere(pos=[0,4,4], radius=0.4)
+
+center1 = sphere(radius=0.4, pos=[2,0,2],color=color.blue)
+center2 = sphere(radius=0.4, pos=[0,2,2],color=color.blue)
+center3 = sphere(radius=0.4, pos=[4,2,2],color=color.blue)
+center4 = sphere(radius=0.4, pos=[2,2,0],color=color.blue)
+center5 = sphere(radius=0.4, pos=[2,2,4],color=color.blue)
+center6 = sphere(radius=0.4, pos=[2,4,2],color=color.blue)
